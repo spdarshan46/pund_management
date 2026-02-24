b69d4874579f3833dd276c312ad8c1b2ec04e269
spdarshan46 <spdarshan252@gmail.com>
2026-02-24 16:10:50 +0530

    Updated

--- src/main/java/com/pundmanagement/pundmanagement/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pundmanagement.pundmanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.pundmanagement.pundmanagement.entity.TestEntity;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
