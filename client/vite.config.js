e6c1f3c12b87bd520e69fa2141aa498e37d0f40b
spdarshan46 <spdarshan252@gmail.com>
2026-03-07 18:46:11 +0530

    fix node engine

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 1500
+  },
+
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://pund-management.onrender.com',
         changeOrigin: true,
       }
     }
