d77fa23a2887825398dc4f1d6a3a2b90bf506c69
spdarshan46 <spdarshan252@gmail.com>
2026-03-13 11:24:53 +0530

    clean code

--- server/PundLedger/settings.py
+++ server/PundLedger/settings.py
@@ -150,7 +150,13 @@ SIMPLE_JWT = {
 }
 
 # ── Email ─────────────────────────────────────────────────────
-RESEND_API_KEY = config("RESEND_API_KEY")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 FRONTEND_URL = "https://pundx.vercel.app"
 
